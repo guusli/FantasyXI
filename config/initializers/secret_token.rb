37ca739d9fc1611f82cbcd0682927084c76afa2e
Gustav Lindbergh <g.lindbergh@gmail.com>
2012-03-23 08:40:16 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasyxi::Application.config.secret_token = '934881be73176578d6c1d3731fb4fe0ea457b98fcbae47e3812b799a1afdcd4db4bf92862c1e95b1689989d53ce810f49fc71d4d6c57123347acfd051e74c265'
