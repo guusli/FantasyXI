f5c8b6a743607b766fcf29e82c77de610a2a7c81
Jens Lund <lund.jens@hotmail.com>
2012-03-25 20:10:18 +0200

    Ändrade lite småfel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def sortable(column, title = nil)
 	  title ||= column.titleize
-	  direction = column == sort_column && sort_direction== "asc" ? "desc" : "asc"
+	  direction = column == sort_column && sort_direction== "asc" ? "desc" : "desc"
 	  link_to title, params.merge(:sort => column, :direction => direction, :page => nil)
 	end
 end
