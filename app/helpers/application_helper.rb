53465ed4724c4a93726c9e840ff263abfdab0a9a
Jens Lund <lund.jens@hotmail.com>
2012-03-26 08:47:28 +0200

    Ändrade sorteringen i listan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def sortable(column, title = nil)
 	  title ||= column.titleize
-	  direction = column == sort_column && sort_direction== "asc" ? "desc" : "desc"
+	  direction = column == sort_column && sort_direction== "desc" ? "asc" : "desc"
 	  link_to title, params.merge(:sort => column, :direction => direction, :page => nil)
 	end
 end
