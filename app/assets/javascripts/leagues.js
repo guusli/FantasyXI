4a3f75f0d4356abb0e46ad15c8966ad7d86c0d24
Gustav Lindbergh <g.lindbergh@gmail.com>
2012-05-29 21:35:04 +0200

    Revert "Revert "Prepp inför munta""
    This reverts commit fce0e3d03d4151d3f407bcbf91ccf16064a61384.

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -1,22 +1,21 @@
 $(function() {
 
+// Initierar Twitter-Bootstrap-modal
 $('#myModal').modal({
   		keyboard: false
-	});
+});
 
-	$('#myModal').on('show', function () {
-		
-	});
+// Modal ska vara gömd från start 
+$('#myModal').modal('hide');
 
-	$('#pitch .player .kit').popover();
 
-	$('#myModal').modal('hide')
 
 	$('#invite_table .inviteButton').click(function() {
-		var pressed = $(this);
-		var uid = pressed.data('uid');
+		var pressed = $(this); // knappen som tryckts på
+		var uid = pressed.data('uid'); // facebook id
 		var league_id = pressed.data('leagueid');
-		$(this).button('loading');
+		$(this).button('loading'); // Ändrar state på knappen
+		// Kör send_invite - action
 		$.getScript("/send_invite?uid=" + uid + "&league_id=" + league_id, function(data){
 				pressed.button('complete');
 				pressed.text("Inbjudan skickad.")
